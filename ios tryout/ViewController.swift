23d250a4f78ed1514108aef55db216c0add02105
Isaac T <kevincurry438@gmail.com>
2025-05-04 20:53:16 -0400

    Initial Commit

---
+++ ios tryout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios tryout
+//
+//  Created by T Isaac on 2025/5/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
